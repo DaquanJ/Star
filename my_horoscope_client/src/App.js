7ee8b178b29b736aaa4bb5d4d52faca149ef3a8c
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-25 21:32:24 -0400

    updated component to be rendered

--- my_horoscope_client/src/App.js
+++ my_horoscope_client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Signup from './components/Signup';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Signup/>
     </div>
   );
 }
