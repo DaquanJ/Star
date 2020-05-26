4008fe3f3e9b7db0484ee9998d99bda157102ab5
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-25 23:43:13 -0400

    establishing my horoscopes as a class component

---
+++ my_horoscope_client/src/components/MyHoroscopes.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class MyHoroscopes extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            savedHoroscopes: []
+         }
+    }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default MyHoroscopes;
