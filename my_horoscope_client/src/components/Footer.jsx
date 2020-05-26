6d6de2a3f193d9c2e38595e64c5fd1708355c4c2
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-25 23:03:13 -0400

    established footer as functional component, created ul, established links

---
+++ my_horoscope_client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+
+    return ( 
+        <div>
+            <ul>
+               <Link to='/myhoroscopes' > <li> My Horoscopes </li> </Link> 
+               <Link to='/reviews' > <li> Reviews </li> </Link>
+                <li> &copy; Horoscopes 2020 By Per Scholas </li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Footer;
