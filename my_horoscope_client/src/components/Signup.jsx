3249dee5435afacd6a92a2339bc8fd932e64fd6b
Daquan Johnson <sadu@Daquans-MBP.fios-router.home>
2020-05-25 20:27:43 -0400

    establishing state,sign up form, setting state with signup form values

--- my_horoscope_client/src/components/Signup.jsx
+++ my_horoscope_client/src/components/Signup.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
-
 class Signup extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            email: '',
+            userName: '',
+            sign: '',
+            password: ''
+         }
     }
     render() { 
         return ( 
             <div>
-                
+                <form >
+                    <input type="email" placeholder="Email" onChange = {e => this.setState({email: e.target.value})} />
+                    <input type="text" placeholder="User Name" onChange = {e => this.setState({userName: e.target.value})} />
+                    <input type="text" placeholder="Sign" onChange = {e => this.setState({sign: e.target.value})} />
+                    <input type="password" placeholder="Password" onChange = {e => this.setState({password: e.target.value})} />
+                    <button type="submit" > Sign Up! </button>
+                </form>
             </div>
          );
     }
