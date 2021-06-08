f74c642f776201392f27a3d9038bc41c8a494857
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-07 22:43:46 -0400

    changing things

--- my_horoscope_client/src/App.js
+++ my_horoscope_client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Signup from './components/Signup';
-import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Sign from './components/Sign';
 import DailyHoroscope from './components/DailyHoroscope';
 import MyHoroscopes from './components/MyHoroscopes';
 import Reviews from './components/Reviews';
 import Footer from './components/Footer';
 import './App.css';
+import Horoscope from './components/Horoscope';
 
 function App() {
   return (
     <div className='App'>
-     <Router>
-       <Switch>
-         <Route path='/' exact component={Login} />
-         <Route path='/signup' component={Signup} />
-         <Route path='/horoscope' component={DailyHoroscope} />
-         <Route path='/myhoroscopes' component={MyHoroscopes} />
-         <Route path='/reviews' component={Reviews} />
-       </Switch>
-      <Footer/>
-     </Router>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Sign} />
+          <Route path='/horoscope/:sign' component={Horoscope} />
+          <Route path='/myhoroscopes' component={MyHoroscopes} />
+          <Route path='/reviews' component={Reviews} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
