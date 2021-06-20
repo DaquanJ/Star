ee39e3e9c6134ac7f73ad5808165c6a1f8b32dd8
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-20 16:15:57 -0400

    Updates

---
+++ precache-manifest.37ec3218a1234b1ba18aa735a29f711c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77b87dc52f94e9ee7957d3c2c59d397",
+    "url": "/Star/index.html"
+  },
+  {
+    "revision": "cd08aefb612a561675df",
+    "url": "/Star/static/css/main.cb56de42.chunk.css"
+  },
+  {
+    "revision": "cc0e81f720bbc5e2b3c4",
+    "url": "/Star/static/js/2.705c0143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Star/static/js/2.705c0143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd08aefb612a561675df",
+    "url": "/Star/static/js/main.38c721c6.chunk.js"
+  },
+  {
+    "revision": "0a93cbf0039e0033753f",
+    "url": "/Star/static/js/runtime-main.79fefc43.js"
+  }
+]);
