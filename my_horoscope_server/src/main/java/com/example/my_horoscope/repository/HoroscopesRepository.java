83a8343c94d101b36b5055b67e4c218e5c3afe0a
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-19 21:01:04 -0400

    removing server app

--- my_horoscope_server/src/main/java/com/example/my_horoscope/repository/HoroscopesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.my_horoscope.repository;
-
-import com.example.my_horoscope.model.Horoscopes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HoroscopesRepository extends JpaRepository<Horoscopes, Long> {
-}
