f64be61647489ac98b50bfc7feae3bb7dd48deac
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-20 16:15:34 -0400

    updated readme, configs for json for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Sign from './components/Sign';
 import Footer from './components/Footer';
 import Horoscope from './components/Horoscope';
