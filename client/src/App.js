42cf80450d6f1c996c4617bcfacdd0dbacfff461
Daquan Johnson <Daquanj.Dev@gmail.com>
2021-06-27 23:54:23 -0400

    your star v3 - now using one comp for full app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Sign from './components/Sign';
-import Footer from './components/Footer';
 import Horoscope from './components/Horoscope';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Sign} />
-          <Route path='/horoscope/:sign' component={Horoscope} />
-        </Switch>
-        <Footer />
-      </Router>
+      <Horoscope />
     </div>
   );
 }
--- client/src/components/Horoscope.jsx
+++ client/src/components/Horoscope.jsx
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import '../styles/horoscope.css';
 
 
-const Horoscope = ({ match }) => {
+const Horoscope = () => {
 
-    const [horoscope, setHoroscope] = useState([]);
+    const [sign, setSign] = useState({ signs: "" })
     const [day, setDay] = useState({ day: 'today' });
+    const [horoscope, setHoroscope] = useState([]);
 
-    const getHoroscope = async () => {
-        try {
-            const res = await axios.post(`https://aztro.sameerkumar.website?sign=${match.params.sign}&day=${day.day}`)
-            setHoroscope(res.data)
-            console.log(res.data)
-        } catch (error) {
-            console.error(error)
-        }
+    const getSign = (e) => {
+        setSign({ [e.target.id]: e.target.value })
     }
 
     const getDay = (e) => {
         e.preventDefault();
         const { id, value } = e.target;
         setDay({ [id]: value })
-        console.log(day)
     }
+
+    const getHoroscope = async () => {
+        try {
+            const res = await axios.post(`https://aztro.sameerkumar.website?sign=${sign.signs}&day=${day.day}`)
+            setHoroscope(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
 
     useEffect(() => {
         getHoroscope();
@@ -31,14 +35,20 @@ const Horoscope = ({ match }) => {
 
     return (
         <div>
-            <Link to='/' > <h1 id='app-name' > Your Star </h1> <img style={{ width: '4%', margin: '1% 0% 0% -5%' }} src="https://www.pngkey.com/png/full/182-1828465_picture-black-and-white-zodiac-from-the-box.png" alt="" /> </Link>
+            <h1 id='app-name' > Your Star </h1> <img style={{ width: '4%', margin: '1% 0% 0% -5%' }} src="https://www.pngkey.com/png/full/182-1828465_picture-black-and-white-zodiac-from-the-box.png" alt="" />
             <form className='days' onClick={(e) => getDay(e)} onSubmit={getHoroscope} >
                 <input type="submit" value='Yesterday' id='day' />
                 <input type="submit" value='Today' id='day' />
                 <input type="submit" value='Tomorrow' id='day' />
             </form>
             <div className='zodiac' >
-                <h1 id="title"> {match.params.sign.toUpperCase()} </h1>
+                {/* <h1 id="title"> {match.params.sign.toUpperCase()} </h1> */}
+                <div id='signs-image'>
+                    <img src="https://static.wixstatic.com/media/e6f9d7_1cf63a10d9124730b66cfb48e7683a83~mv2.png/v1/fill/w_504,h_508,al_c,q_85,usm_0.66_1.00_0.01/Zodiac%20Wheel.webp" alt="signs" />
+                </div>
+                <form id='title' onChange={(e) => getSign(e)} onSubmit={getHoroscope} >
+                    <input id='signs' type="text" placeholder="ENTER YOUR SIGN" required />
+                </form>
                 <div className="the-date" >
                     <p id="the-day" > {day.day} </p>
                     <p id="date">  {horoscope.current_date} </p>
@@ -56,9 +66,6 @@ const Horoscope = ({ match }) => {
                     <li id="number"> Lucky Number: {horoscope.lucky_number} </li>
                     <li id="time"> Lucky Time: {horoscope.lucky_time} </li>
                 </ul>
-                <div id='signs-app'>
-                    <img src="https://static.wixstatic.com/media/e6f9d7_1cf63a10d9124730b66cfb48e7683a83~mv2.png/v1/fill/w_504,h_508,al_c,q_85,usm_0.66_1.00_0.01/Zodiac%20Wheel.webp" alt="signs" />
-                </div>
             </div>
         </div>
     );
